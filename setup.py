ccf5865e5784984c312f7389e772f2c1d35f0b48
csaail <87662482+csaail@users.noreply.github.com>
2024-06-01 01:57:00 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypluto',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Saail Chavan',
+    author_email='saailchavan02@gmail.com',
+    description='A library for controlling Pluto drones',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/csaail/pypluto',  # Replace with your GitHub URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
